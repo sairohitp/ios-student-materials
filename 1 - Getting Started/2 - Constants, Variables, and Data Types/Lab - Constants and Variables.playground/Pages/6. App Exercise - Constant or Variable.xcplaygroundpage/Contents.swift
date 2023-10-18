46f93a3e57327d95046b745cce1d8cbca0c5f1cf
Sai Rohit <sairohitp007@gmail.com>
2023-10-18 14:55:43 +0530

    Module 1

---
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+/*:
+## App Exercise - Fitness Tracker: Constant or Variable?
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
+ 
+- Name: The user's name
+- Age: The user's age
+- Number of steps taken today: The number of steps that a user has taken today
+- Goal number of steps: The user's goal for number of steps to take each day
+- Average heart rate: The user's average heart rate over the last 24 hours
+ */
+let userName = "Jakob"
+print("The name of a person after creating his fitness account remains constant")
+
+var Age = 25
+print("A persons age keeps changing over the years")
+
+var stepsTakenToday = 2000
+print("The number of steps taken by a person constantly keeps changing during the day")
+
+let stepsGoal = 5000
+print("A goal is a fixed outcome the user is expecting to achieve.")
+
+var averageHeartrate = 80
+print("The human heart is very versatile and hence it keeps changing dynamically thoughout the day.")
+
+/*:
+ Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
+ 
+[Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Types and Type Safety](@next)
+ */
